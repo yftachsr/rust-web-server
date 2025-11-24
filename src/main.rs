e9164e54a3aab9d220b0ae43f67a82e540e8a23f
yftach <yftacsr@gmail.com>
2025-11-24 13:10:23 +0200

    listening to TCP connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{io::Read, net::TcpListener};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:8080").expect("Unable to bind address");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => { 
+                println!("Connection established");
+                println!("{:?}", stream.bytes());
+                //handle_connection(stream); 
+            }
+            Err(e) => { eprintln!("{}", e); }
+        }
+    }
 }
