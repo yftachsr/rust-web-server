84985335a181cbb0a9a6d9ad20efbf8df8550f9a
yftach <yftacsr@gmail.com>
2025-12-01 11:51:14 +0200

    Implementing a threadpool

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,48 @@
+use std::{sync::{mpsc, Arc, Mutex}, thread};
 
-pub struct ThreadPool;
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>
+}
+
+pub struct ThreadPool {
+    threads: Vec<Worker>,
+    sender: mpsc::Sender<Job>
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+                println!("Worker {id} executing");
+                job();
+            }
+        });
+        Worker { id, thread }
+        //Worker { id, thread: thread::spawn(|| { receiver }) }
+    }
+}
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut threads: Vec<Worker> = Vec::with_capacity(size);
+        for i in 0..size {
+            threads.push(Worker::new(i, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { threads, sender }
     }
 
     pub fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static {
-
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
